b1fde51dfd8b40bac98ede6f2cff857c533fbeed
gscar_000 <gscar_000@GABRIEL>
2023-11-29 00:07:08 -0500

    Add project files.

---
+++ ExpensesCalculator.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpensesVisualizer
+{
+    internal class ExpensesCalculator
+    {
+
+        private double housePayments;
+        private double carPayments;
+        private double utilities;
+        private double food;
+        private double clothing;
+        private double medical;
+        private double homeInsurance;
+        private double entertainment;
+        private double miscellaneous;
+        private double totalExpenses;
+        private double[] monthlyExpenses;
+        private double[] yearlyExpenses;
+
+
+        // constructor
+
+        public ExpensesCalculator()
+        {
+            housePayments = 0;
+            carPayments = 0;
+            utilities = 0;
+            food = 0;
+            clothing = 0;
+            medical = 0;
+            homeInsurance = 0;
+            entertainment = 0;
+            miscellaneous = 0;
+            totalExpenses = 0;
+            monthlyExpenses = new double[12];
+            yearlyExpenses = new double[12];
+        }
+
+        // all the getters and setters for the variables
+        public double HousePayments
+        {
+            get
+            {
+                return housePayments;
+            }
+            set
+            {
+                housePayments = value;
+            }
+        }
+    public double CarPayments
+    {
+        get
+        {
+            return carPayments;
+        }
+        set
+        {
+            carPayments = value;
+        }
+    }
+    public double Utilities
+    {
+
+        get
+        {
+            return utilities;
+        }
+        set
+        {
+            utilities = value;
+        }
+    }
+    public double Food
+    {
+
+        get
+        {
+            return food;
+        }
+        set
+        {
+            food = value;
+        }
+    }
+    public double Clothing
+    {
+
+        get
+        {
+            return clothing;
+        }
+        set
+        {
+            clothing = value;
+        }
+    }
+    public double Medical
+    {
+
+        get
+        {
+            return medical;
+        }
+        set
+        {
+            medical = value;
+        }
+    }
+    public double HomeInsurance
+    {
+
+        get
+        {
+            return homeInsurance;
+        }
+        set
+        {
+            homeInsurance = value;
+        }
+    }
+    public double Entertainment
+    {
+
+        get
+        {
+            return entertainment;
+        }
+        set
+        {
+            entertainment = value;
+        }
+    }
+    public double Miscellaneous
+    {
+
+        get
+        {
+            return miscellaneous;
+        }
+        set
+        {
+            miscellaneous = value;
+        }
+    }
+    public double TotalExpenses
+    {
+
+        get
+        {
+            return totalExpenses;
+        }
+        set
+        {
+            totalExpenses = value;
+        }
+    }
+        public double[] MonthlyExpenses
+        {
+
+        get
+            {
+            return monthlyExpenses;
+        }
+        set
+            {
+            monthlyExpenses = value;
+        }
+        }
+
+        public double[] YearlyExpenses
+        {
+            get { return yearlyExpenses;
+            }
+            set
+            {
+                yearlyExpenses = value;
+            }
+        }
+
+        public void calculateMonthlyExpenses()
+        {
+            totalExpenses = housePayments + carPayments + utilities + food + clothing + medical + homeInsurance + entertainment + miscellaneous;
+
+            // to get the percent of each expense
+            for (int i = 0; i < monthlyExpenses.Length; i++)
+            {
+                monthlyExpenses[i] = monthlyExpenses[i] / totalExpenses;
+            }
+
+           
+        }
+
+        public void calculateYearlyExpenses()
+        {
+
+        }
+
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace ExpensesVisualizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            textBox1 = new TextBox();
+            txtUserName = new TextBox();
+            txtPassword = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            label2 = new Label();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(144, 75);
+            label1.Name = "label1";
+            label1.Size = new Size(48, 15);
+            label1.TabIndex = 0;
+            label1.Text = "LOG IN ";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Black;
+            panel1.Location = new Point(114, 155);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(100, 1);
+            panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Black;
+            panel2.Controls.Add(textBox1);
+            panel2.Location = new Point(114, 243);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(100, 1);
+            panel2.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(0, 0);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // txtUserName
+            // 
+            txtUserName.Location = new Point(114, 130);
+            txtUserName.Multiline = true;
+            txtUserName.Name = "txtUserName";
+            txtUserName.Size = new Size(100, 23);
+            txtUserName.TabIndex = 3;
+            txtUserName.TextChanged += txtUserName_TextChanged;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(114, 214);
+            txtPassword.Multiline = true;
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(100, 23);
+            txtPassword.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(130, 331);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 5;
+            button1.Text = "LOG IN ";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(130, 373);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 24);
+            button2.TabIndex = 6;
+            button2.Text = "SIGN UP";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(144, 434);
+            label2.Name = "label2";
+            label2.Size = new Size(29, 15);
+            label2.TabIndex = 7;
+            label2.Text = "EXIT";
+            label2.Click += label2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(348, 480);
+            Controls.Add(label2);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUserName);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Panel panel1;
+        private Panel panel2;
+        private TextBox textBox1;
+        private TextBox txtUserName;
+        private TextBox txtPassword;
+        private Button button1;
+        private Button button2;
+        private Label label2;
+    }
+}
